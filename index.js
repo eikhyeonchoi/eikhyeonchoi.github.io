582a7502fa5aab5137e16d79f93d5c60493ede39
eikhyeonchoi <eikhyeon.choi@gmail.com>
2019-07-17 17:20:01 +0900

    deploy

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+  new WOW().init();
+  
+  $('#fullpage').fullpage({
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    paddingTop: '70px',
+    navigation: true,
+    navigationPosition: "right",
+    scrollOverflow: true,
+    scrollHorizontally: false
+  });
+  
+  var typed = new Typed('.greeting-text', {
+    strings: ['Web Developer 최익현의 포트폴리오 입니다.'],
+    typeSpeed: 120,
+    showCursor: false,
+  })
+}); // ready
+
+$('.menu').click(function(e) {
+  var targetText = $(e.target).text();
+  if (targetText.includes('Home')) {
+    fullpage_api.moveTo(1);
+  } else if (targetText.includes('About')) {
+    fullpage_api.moveTo(2);
+  } else if (targetText.includes('Projects')) {
+    fullpage_api.moveTo(3);
+  } 
+}) // click
+
+
+
+
+
