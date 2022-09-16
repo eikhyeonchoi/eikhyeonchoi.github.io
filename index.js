660c81ee2db53142d24fd631aee1f145b95b1cff
ikhyunchoi <eikhyeon.choi@gmail.com>
2022-09-16 11:07:14 +0900

    update
    update

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-/**
- * 
- */
-$(document).ready(function() {
-  new WOW().init();
+// /**
+//  * 
+//  */
+// $(document).ready(function() {
+//   new WOW().init();
   
-  $('#fullpage').fullpage({
-    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-    navigation: true,
-    navigationPosition: "right",
-    scrollOverflow: true,
-    scrollHorizontally: false
-  });
+//   $('#fullpage').fullpage({
+//     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+//     navigation: true,
+//     navigationPosition: "right",
+//     scrollOverflow: true,
+//     scrollHorizontally: false
+//   });
   
-  var typed = new Typed('.greeting-text', {
-    strings: ['Web Developer 최익현의 포트폴리오입니다'],
-    typeSpeed: 120,
-    showCursor: false,
-  })
-}); // ready
+//   var typed = new Typed('.greeting-text', {
+//     strings: ['Web Developer 최익현의 포트폴리오입니다'],
+//     typeSpeed: 120,
+//     showCursor: false,
+//   })
+// }); // ready
 
-$('.menu').click(function(e) {
-  var targetText = $(e.target).text();
-  if (targetText.includes('Home')) {
-    fullpage_api.moveTo(1);
-  } else if (targetText.includes('About')) {
-    fullpage_api.moveTo(2);
-  } else if (targetText.includes('Projects')) {
-    fullpage_api.moveTo(3);
-  } 
-}) // click
+// $('.menu').click(function(e) {
+//   var targetText = $(e.target).text();
+//   if (targetText.includes('Home')) {
+//     fullpage_api.moveTo(1);
+//   } else if (targetText.includes('About')) {
+//     fullpage_api.moveTo(2);
+//   } else if (targetText.includes('Projects')) {
+//     fullpage_api.moveTo(3);
+//   } 
+// }) // click
 
 
-$('.skill').hover(function(e) {
-  $(e.target).children().first().addClass('visible').removeClass('invisible');
+// $('.skill').hover(function(e) {
+//   $(e.target).children().first().addClass('visible').removeClass('invisible');
   
-  },function(e) {
-    $(e.target).addClass('invisible').removeClass('visible');
-    if($(this).hasClass('invisible')) {
-      $(this).removeClass('invisible');
-    }
-});
+//   },function(e) {
+//     $(e.target).addClass('invisible').removeClass('visible');
+//     if($(this).hasClass('invisible')) {
+//       $(this).removeClass('invisible');
+//     }
+// });
 
 
